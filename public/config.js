b1f33d96b83ed9f7429ba5f2fb6ca2440c8af09d
ms-yogi <bende.yogini@gmail.com>
2020-12-27 12:18:10 +0530

    [setup] Added config setup and basic styling

--- public/config.js
+++ public/config.js
@@ -1,3 +1,4 @@
 /* eslint-disable no-unused-vars */
 // all configurations
 var BASE_URL = '//your-base-api-url';
+var SOURCE = 'WEB_APP';
--- src/pages/Login.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Login = () => {
-	return <div>Login component</div>;
-};
-
-export default Login;
